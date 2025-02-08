e14fbb5f6477ad19394b6d164ab95b05bda7bc76
Meleshkov_alex <dienteen@gmail.com>
2025-02-08 12:16:54 +0300

    add block_sheets animation

--- res/scripts/mine_block.gd
+++ res/scripts/mine_block.gd
@@ -29,24 +29,45 @@ func _process(_delta):
 			#block_hp -= randf_range(10,50)
 			#print(block_hp)
 
-func _block_tick():
+
 	
-	$Sprite2D.show()
-	$mine_block_animation.play ("mine_block")
+func _block_tick():
+	var frame = "res://Scenes/mine_block.tscn"
+	if block_hp > 90:
+		$AnimatedSprite2D.set_frame(0)
+	if block_hp < 90:
+		$AnimatedSprite2D.set_frame(1)
+	if block_hp < 70:
+		$AnimatedSprite2D.set_frame(2)
+	if block_hp < 40:
+		$AnimatedSprite2D.set_frame(3)
+	if block_hp < 10:
+		$AnimatedSprite2D.set_frame(4)
+	#$Sprite2D.show()
+	#$AnimatedSprite2D.show
+	$Block_mine_animation.play("block_mine")
+	#$mine_block_animation.play ("mine_block")
 	block_hp -= randf_range(1,15)
+	
 	if block_hp <= 0:
 		block_live = false
-		$Sprite2D.hide()
+		#$Sprite2D.hide()
+		$AnimatedSprite2D.hide()
 		$CPUParticles2D.emitting = true
 		await get_tree().create_timer(0.2).timeout  # Ждём 1 секунду
 		$CPUParticles2D.emitting = false
+		
 		block_live = true
-		$Sprite2D.show()
+		$AnimatedSprite2D.set_frame(0)
+		$AnimatedSprite2D.show()
+		#$Sprite2D.show()
 		$AudioStreamPlayer2D.play()
 		
 	if block_hp > 100:
 		block_hp = 100
 	#print(block_hp)
+
+	
 	
 func _particles_tick():
 	pass
